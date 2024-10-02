6d328615eb70578ecff015808f26cbf2a8d4f355
Santiago Robayo <santiago.robayo@vindow.com>
2024-10-01 21:52:31 -0500

    Initial Commit Splash Screen

---
+++ app/src/main/java/com/retie/retieApp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.retie.retieApp
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+    }
+}
