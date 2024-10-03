42b31945b728c7907aa5b7c4f77cc3c79393cf6b
Santiago Robayo <santiago.robayo@vindow.com>
2024-10-03 00:17:48 -0500

    Navigation Drawer

--- app/src/main/java/com/retie/retieApp/MainActivity.kt
+++ app/src/main/java/com/retie/retieApp/MainActivity.kt
@@ -1,14 +1,53 @@
 package com.retie.retieApp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> Toast.makeText(applicationContext, "Click Inicio", Toast.LENGTH_SHORT).show()
+                R.id.nav_caps -> Toast.makeText(applicationContext, "Click Capitulos", Toast.LENGTH_SHORT).show()
+                R.id.nav_titles -> Toast.makeText(applicationContext, "Click Titulos", Toast.LENGTH_SHORT).show()
+                R.id.nav_articles -> Toast.makeText(applicationContext, "Click Articulos", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
